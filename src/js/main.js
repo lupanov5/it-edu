8c896eda5ed5601f4508451b2765fe68002dab32
Dmitry <lupanov5@mail.ru>
2021-10-18 12:24:36 +0300

    Implemented main slider

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import Slider from "./modules/slider";
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    "use strict";
+    
+    const slider = new Slider('.page', '.next');
+    slider.render();
+
+});
